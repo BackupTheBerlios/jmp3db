de93bf1f99056dce71b372b35ac110c7af011406
einfachnuralex <einfachnuralex>
2004-08-24 14:45:32 +0000

    Klasse überarbeitet / auf MP3Song Interface umgestellt

--- jmp3db/src/de/mp3db/util/Tag.java
+++ jmp3db/src/de/mp3db/util/Tag.java
@@ -10,9 +10,12 @@ import java.io.File;
  *      
  *      @author $Author: einfachnuralex $
  *
- *      @version $Id: Tag.java,v 1.3 2004/08/24 12:48:04 einfachnuralex Exp $
+ *      @version $Id: Tag.java,v 1.4 2004/08/24 14:45:32 einfachnuralex Exp $
  *  
  *      $Log: Tag.java,v $
+ *      Revision 1.4  2004/08/24 14:45:32  einfachnuralex
+ *      Klasse überarbeitet / auf MP3Song Interface umgestellt
+ *
  *      Revision 1.3  2004/08/24 12:48:04  einfachnuralex
  *      CVS Kommentare eingefügt
  *      parseTag(String) geändert
@@ -21,198 +24,75 @@ import java.io.File;
  */
 public class Tag {
 
-	public static int OGG = 1;
-	public static int MP3 = 2;
+	private MP3Song song;
 	
-	private int     ID;
-	private String  title;
-	private String  artist;
-	private String  album;
-	private int     year;
-	private String  genre;
-	private int     trackNo;
-	private int     bitrate;
-	private int     length;
-	private long    fileSize;
-	private String  fileName;
-	private long    lastModified;
-    
-	private int codec;
-
-	public Tag(MP3Song song) {
-		ID = song.getID();
-		title = song.getTitle();
-		artist = song.getArtist();
-		album = song.getAlbum();
-		year = song.getYear();
-		genre = song.getGenre();
-		trackNo = song.getTrackNo();
-		bitrate = song.getBitrate();
-		length = song.getLength();
-		fileSize = song.getFileSize();
-		fileName = song.getFileName();
-		lastModified = song.getLastModified();
+	public Tag(MP3Song newSong) {
+		song = newSong;
 	}
 	
 	public Tag(String file) {
 		this.parseTag(file);
-		
 	}
 
+	public MP3Song getSong() {
+		return song;
+	}
+	
 	private void parseTag(String fileName) {
 		if(fileName.substring(fileName.length()-4, fileName.length()).equalsIgnoreCase(".mp3")) {
-		codec = MP3;
 			try {
 				MP3File file = new MP3File(new File(fileName));
-				title = new String(file.getTitle());
-				artist = new String(file.getArtist());
-				album = new String(file.getAlbum());
+				song = new MP3Song();
+				
+				//song.setID(this.getMaxSongId()+1);
+				song.setTitle(file.getTitle());
+				song.setArtist(file.getArtist());
+				song.setAlbum(file.getAlbum());
 				try {
-					year = Integer.parseInt(file.getYear());
+					song.setYear(Integer.parseInt(file.getYear()));
 				}
 				catch(NumberFormatException ex) {
-					year = 0;
+					song.setYear(0);
 				}
-				genre = file.getGenre();
-				trackNo = file.getTrack();
-				bitrate = file.getBitRate();
-				length = (int)file.getPlayingTime();
-				fileSize = file.getFileSize();
-				this.fileName = new String(fileName);
-				lastModified = new File(fileName).lastModified();
-				
+				song.setGenre(file.getGenre());
+				song.setTrackNo(file.getTrack());
+				song.setBitrate(file.getBitRate());
+				song.setLength((int)file.getPlayingTime());
+				song.setFileSize(file.getFileSize());
+				song.setFileName(fileName);
+				song.setLastModified(new File(fileName).lastModified());
+				song.setHashCode(new File(fileName).hashCode());
+				song.setCodec(MP3Song.MP3);				
 			}
 			catch(Exception ex) {
 				System.out.println(ex);
 			}
 		}
 		else if(fileName.substring(fileName.length()-4, fileName.length()).equalsIgnoreCase(".ogg")) {
-			
+			// not complete
 		}
-		 		
 	}
 	
 	public boolean writeTag() {
-	boolean success = false;
-		try {
-			MP3File file = new MP3File(new File(fileName));
-			file.setTitle(title);
-			file.setArtist(artist);
-			file.setAlbum(album);
-			file.setYear(String.valueOf(year));
-			file.setGenre(genre);
-			file.setTrack(trackNo);
+		boolean success = false;
 
-			file.writeTags();
-			success = true;
-		}
-		catch(Exception ex) {
+		if(song != null) {
+			try {
+				MP3File file = new MP3File(new File(song.getFileName()));
+				file.setTitle(song.getTitle());
+				file.setArtist(song.getArtist());
+				file.setAlbum(song.getAlbum());
+				file.setYear(String.valueOf(song.getYear()));
+				file.setGenre(song.getGenre());
+				file.setTrack(song.getTrackNo());
+	
+				file.writeTags();
+				success = true;
+			}
+			catch(Exception ex) {
+			}
 		}
 		
 		return success;
-	}
-    
-	public void setTitle(String title) {
-		this.title = title;
-	}
-    
-	public void setID(int id) {
-		this.ID = id;
-	}
-    
-	public int getID() {
-		return this.ID;
-	}
-    
-	public String getTitle() {
-		return this.title;
-	}
-    
-	public void setArtist(String artist) {
-		this.artist = artist;
-	}
-    
-	public String getArtist() {
-		return this.artist;
-	}
-    
-	public void setAlbum(String album) {
-		this.album = album;
-	}
-    
-	public String getAlbum() {
-		return this.album;
-	}
-    
-	public void setYear(int year) {
-		this.year = year;
-	}
-    
-	public int getYear() {
-		return this.year;
-	}
-    
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-    
-	public String getGenre() {
-		return this.genre;
-	}
-    
-	public void setTrackNo(int trackNo) {
-		this.trackNo = trackNo;
-	}
-    
-	public int getTrackNo() {
-		return this.trackNo;
-	}
-    
-	public void setBitrate(int bitrate) {
-		this.bitrate = bitrate;
-	}
-    
-	public int getBitrate() {
-		return this.bitrate;
-	}
-    
-	public void setLength(int length) {
-		this.length = length;
-	}
-    
-	public int getLength() {
-		return this.length;
-	}
-    
-	public void setFileSize(long fileSize) {
-		this.fileSize = fileSize;
-	}
-    
-	public long getFileSize() {
-		return this.fileSize;
-	}
-    
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-    
-	public String getFileName() {
-		return this.fileName;
-	}
-    
-	public void setLastModified(long lastModified) {
-		this.lastModified = lastModified;
-	}
-    
-	public long getLastModified() {
-		return this.lastModified;
-	}
-    
-	public void setCodec(int codec) {
-		this.codec = codec;
-	}
-    
-	public int getCodec() {
-		return codec;
 	}
 }
