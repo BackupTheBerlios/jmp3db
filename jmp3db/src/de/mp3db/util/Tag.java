4916815b2cf6c5acc052a97eeb8e6bb0bfc330e4
einfachnuralex <einfachnuralex>
2004-08-25 15:15:08 +0000

    Ogg Support implementiert

--- jmp3db/src/de/mp3db/util/Tag.java
+++ jmp3db/src/de/mp3db/util/Tag.java
@@ -4,15 +4,21 @@ package de.mp3db.util;
 
 import helliker.id3.MP3File;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**     Klasse dient zum auslesen der TAG Informationen
  *      
  *      @author $Author: einfachnuralex $
  *
- *      @version $Id: Tag.java,v 1.4 2004/08/24 14:45:32 einfachnuralex Exp $
+ *      @version $Id: Tag.java,v 1.5 2004/08/25 15:15:08 einfachnuralex Exp $
  *  
  *      $Log: Tag.java,v $
+ *      Revision 1.5  2004/08/25 15:15:08  einfachnuralex
+ *      Ogg Support implementiert
+ *
  *      Revision 1.4  2004/08/24 14:45:32  einfachnuralex
  *      Klasse überarbeitet / auf MP3Song Interface umgestellt
  *
@@ -69,7 +75,58 @@ public class Tag {
 			}
 		}
 		else if(fileName.substring(fileName.length()-4, fileName.length()).equalsIgnoreCase(".ogg")) {
-			// not complete
+			try {
+				VorbisInfo vorbis = new VorbisInfo(new BufferedInputStream(new FileInputStream(fileName)));
+				
+				song = new MP3Song();				
+				//System.out.println(vorbis.toString());
+				if(vorbis.getComments("title") != null) {
+					song.setTitle((String)vorbis.getComments("title").get(0));
+				}
+				else {
+					song.setTitle("");
+				}
+				if(vorbis.getComments("artist") != null) {
+					song.setArtist((String)vorbis.getComments("artist").get(0));
+				}
+				else {
+					song.setArtist("");
+				}
+				if(vorbis.getComments("album") != null) {
+					song.setAlbum((String)vorbis.getComments("album").get(0));
+				}
+				else {
+					song.setAlbum("");
+				}
+				if(vorbis.getComments("date") != null) {
+					song.setYear(Integer.parseInt((String)vorbis.getComments("date").get(0)));
+				}
+				else {
+					song.setYear(0);
+				}
+				if(vorbis.getComments("genre") != null) {
+					song.setGenre((String)vorbis.getComments("genre").get(0));
+				}
+				else {
+					song.setGenre("");
+				}
+				if(vorbis.getComments("tracknumber") != null) {
+					song.setTrackNo(Integer.parseInt((String)vorbis.getComments("tracknumber").get(0)));
+				}
+				else {
+					song.setTrackNo(0);
+				}
+				song.setBitrate((int)vorbis.getBitrate());
+				song.setLength(0);
+				song.setFileSize(new File(fileName).length());
+				song.setFileName(fileName);
+				song.setLastModified(new File(fileName).lastModified());
+				song.setHashCode(new File(fileName).hashCode());
+				song.setCodec(MP3Song.OGG);				
+			}
+			catch(IOException ex) {
+				System.out.println(ex);
+			}
 		}
 	}
 	
