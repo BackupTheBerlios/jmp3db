20b9f18554e532a971a1c18fc9ef0bb6a3b3956f
einfachnuralex <einfachnuralex>
2004-10-29 11:10:18 +0000

    search() hinzugefügt

--- jmp3db/src/de/mp3db/MultiPanel.java
+++ jmp3db/src/de/mp3db/MultiPanel.java
@@ -1,7 +1,10 @@
 package de.mp3db;
 
+import java.util.Vector;
+
 import de.mp3db.gui.MP3MultiPanel;
 import de.mp3db.gui.ManagerGUI;
+import de.mp3db.util.FuzzySearch;
 import de.mp3db.util.MP3Song;
 
 /**
@@ -42,5 +45,18 @@ public class MultiPanel {
 	
 	public MP3Song changedSong() {
 		return panel.getChangedSong();
+	}
+	
+	public void search() {
+		Vector searchResult = new Vector();
+		Vector tmp = DBTree.getInstance().getActSongs();
+		for(int i = 0; i < tmp.size(); i++) {
+			FuzzySearch obj = new FuzzySearch(((MP3Song)tmp.get(i)).toString(), panel.getSearchString(), 12);
+			double result = obj.getResult();
+			if(result > 0) {
+				searchResult.add((MP3Song)tmp.get(i));
+			}
+		}
+		Mainlist.getInstance().add(searchResult);
 	}
 }
